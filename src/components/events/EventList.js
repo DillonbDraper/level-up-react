dd5f59fd7d2e5179bfc8c16de3dc9b4d19677701
Dillon Draper <dillonbdraper@gmail.com>
2021-02-09 14:04:39 -0600

    Probably should have been more specific with branch name.  Added a function that signs users up for events in the database and created buttons to do it attached to each event in EventList

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,11 +1,10 @@
 import React, { useContext, useEffect } from "react"
 import { EventContext } from "./EventProvider.js"
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom"
 
-
-export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+export const EventList = () => {
     const history = useHistory()
+    const { events, getEvents, joinEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -15,31 +14,27 @@ export const EventList = (props) => {
         <article className="events">
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
             </header>
             {
                 events.map(event => {
+                    const attending = events.some(evt => evt.id === event.id)
                     return <section key={event.id} className="registration">
                         <div className="registration__game">{event.game.title}</div>
                         <div>{event.description}</div>
                         <div>
-                            {
-                                new Date(event.date).toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
-                             } {event.time}
+                            {event.date} @ {event.time}
                         </div>
+                        <button className="btn btn-2"
+                            onClick={() => joinEvent(event.id)}
+                        >Join</button>
                     </section>
                 })
             }
-            <button className="btn btn-2 btn-sep icon-create"
-                onClick={() => {
-                    history.push({ pathname: "/events/new" })
-                }}
-            >Register New Event</button>
         </article >
     )
 }
