814833fe6a3ceda16a0c4b8c956174294ab3f0e8
Dillon Draper <dillonbdraper@gmail.com>
2021-02-09 15:49:31 -0600

    Updated EventList and EventProvider to supporter toggling between leaving/joining events

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -4,12 +4,13 @@ import { useHistory } from "react-router-dom"
 
 export const EventList = () => {
     const history = useHistory()
-    const { events, getEvents, joinEvent } = useContext(EventContext)
+    const { events, getEvents, joinEvent, leaveEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
     }, [])
 
+    
     return (
         <article className="events">
             <header className="events__header">
@@ -22,16 +23,21 @@ export const EventList = () => {
             </header>
             {
                 events.map(event => {
-                    const attending = events.some(evt => evt.id === event.id)
                     return <section key={event.id} className="registration">
                         <div className="registration__game">{event.game.title}</div>
                         <div>{event.description}</div>
                         <div>
                             {event.date} @ {event.time}
                         </div>
-                        <button className="btn btn-2"
-                            onClick={() => joinEvent(event.id)}
-                        >Join</button>
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id)}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id)}
+                                    >Join</button>
+                        }
                     </section>
                 })
             }
